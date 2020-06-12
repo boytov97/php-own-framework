f436730493ac3e7daa791ab2a9a6e963c47210ec
turarbek <turarbek.boyutov@gmail.com>
2020-06-12 23:34:41 +0600

    added htaccess file for apache and fixed database config

--- vendor/Database/Db.php
+++ vendor/Database/Db.php
@@ -17,7 +17,7 @@ class Db
   {
     $dbHost = array_key_exists('host', $this->mysqlConfig) ? $this->mysqlConfig['host'] : '';
     $dbName = array_key_exists('database', $this->mysqlConfig) ? $this->mysqlConfig['database'] : '';
-    $dbUser = array_key_exists('user', $this->mysqlConfig) ? $this->mysqlConfig['user'] : '';
+    $dbUser = array_key_exists('username', $this->mysqlConfig) ? $this->mysqlConfig['username'] : '';
     $dbPassword = array_key_exists('password', $this->mysqlConfig) ? $this->mysqlConfig['password'] : '';
 
     return new PDO("mysql:host=".$dbHost."; dbname=".$dbName, $dbUser, $dbPassword);
